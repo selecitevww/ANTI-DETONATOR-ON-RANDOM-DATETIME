fa9da6d44e26dee8e540a5b5dbe02b661891bbfa
nyuiop <gapin.sla@gmail.com>
2025-08-29 03:56:47 +0300

    Update datarandrr2 — копия — копия.bat

--- HIGH-YEAR-INCLUDE/datarandrr2 — копия — копия.bat
+++ HIGH-YEAR-INCLUDE/datarandrr2 — копия — копия.bat
@@ -1,7 +1,12 @@
 @echo off
 
+REM      BATCH FILE
+
 chcp 65001
 
+cls
+
+color 70
 
 setlocal EnableDelayedExpansion
 
@@ -17,13 +22,13 @@ set /a "var1=randomString" && cls
 SET var2=3 && cls
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress1"="%randomString%" && cls
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress1"="%randomString%" && cls
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress1"="0" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=21234567821"
 set "length=1"
@@ -36,13 +41,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress2"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress2"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress2"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=101010101010"
 set "length=1"
@@ -63,13 +68,13 @@ SET var4=0
 IF %var1% GTR %var4% (
     SET /A "NetworkAddress3=0"
     SET /A "NetworkAddress4"="%randomStringg%"  
-    ECHO %var3% больше %var4%.
+    ECHO %var3% ГЎГ®Г«ГјГёГҐ %var4%.
 ) ELSE IF %var3% LSS %var4% (
-    ECHO %var3% меньше %var4%.
+    ECHO %var3% Г¬ГҐГ­ГјГёГҐ %var4%.
 ) ELSE (
     SET /A "NetworkAddress3=0"
     SET /A "NetworkAddress4"="%randomStringg%" 
-    ECHO %var4% равно %var4%.
+    ECHO %var4% Г°Г ГўГ­Г® %var4%.
 )
 set "charset=222222222222"
 set "length=1"
@@ -82,13 +87,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress5"="%randomString%"  
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress5"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress5"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=000000000"
 set "length=1"
@@ -101,13 +106,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress6"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress6"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress6"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=0123456765"
 set "length=1"
@@ -120,13 +125,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress7"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress7"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress7"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=012345654301"
 set "length=1"
@@ -140,14 +145,14 @@ SET var2=5
 ::8
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress8"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress8"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 
 ) ELSE (
     SET /A "NetworkAddress8"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charsetx=0120120120120"
 set "lengthx=1"
@@ -183,15 +188,15 @@ SET kar6=2
 IF %kar5% GTR %kar6% (
     SET /A "NetworkAddress9=%randomStringx%" 
     SET /A "NetworkAddres8=%randomStringsx%"
-    ECHO %kar5% больше %kar6%.
+    ECHO %kar5% ГЎГ®Г«ГјГёГҐ %kar6%.
 ) ELSE IF %kar5% LSS %kar6% (    
     SET /A "NetworkAddress9=%randomStringxd%" 
     SET /A "NetworkAddres8"="%randomString%" 
-    ECHO %kar5% меньше %kar6%.
+    ECHO %kar5% Г¬ГҐГ­ГјГёГҐ %kar6%.
 ) ELSE (
     SET /A "NetworkAddress9=%randomStringxd%" 
     SET /A "NetworkAddres8=randomStringsx" 
-    ECHO %kar5% равно %kar6%.
+    ECHO %kar5% Г°Г ГўГ­Г® %kar6%.
 )
 set "charset=012345432101"
 set "length=1"
@@ -204,14 +209,14 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres7"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres7"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddres7"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 set "charset=01234567890123"
@@ -227,14 +232,14 @@ SET var2=5
 ::6
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres6"="%randomString%"  
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres6"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddres6"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=0123454320123"
 set "length=1"
@@ -247,14 +252,14 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres5"="%randomString%"  
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres5"="%randomString%"  
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddres5"="%randomString%"  
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=012345432101234"
 set "length=1"
@@ -267,13 +272,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /a "NetworkAddressk"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /a "NetworkAddressk"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /a  "NetworkAddressk"="%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 ECHO %NetworkAddress1%%NetworkAddress2%-%NetworkAddress3%%NetworkAddress4%-%NetworkAddress5%%NetworkAddress6%%NetworkAddress7%%NetworkAddress8%
 date %NetworkAddress1%%NetworkAddress2%-%NetworkAddress3%%NetworkAddress4%-%NetworkAddress5%%NetworkAddress6%%NetworkAddress7%%NetworkAddress8%
@@ -288,13 +293,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress16"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress16"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress16"="%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 set "charset=0123456789012"
@@ -308,13 +313,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress17"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress17"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress17"="%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=0123456789012"
 set "length=1"
@@ -327,13 +332,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres8"=%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres8"=%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddres8"=%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 echo %NetworkAddress9%%NetworkAddres8%:%NetworkAddres7%%NetworkAddres6%:%NetworkAddres5%%NetworkAddress17%,%NetworkAddressk%%NetworkAddress16% 
 set "charset=01230123201230"
@@ -347,13 +352,13 @@ set /a "var1=randomString"
 SET var2=3 && cls
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress1"="%randomString%" && cls
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress1"="%randomString%" && cls
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress1"="0" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=2123456789212"
 set "length=1"
@@ -366,13 +371,13 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress2"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress2"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress2"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=22222222222"
 set "length=1"
@@ -385,13 +390,13 @@ set /a "var1=randomString"
 SET var2=2 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress5"="2"  
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress5"="2" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress5"="2" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=000000000"
 set "length=1"
@@ -404,13 +409,13 @@ set /a "var1=randomString"
 SET var2=0 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress6"="0" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress6"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress6"="0" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=01234567650"
 set "length=1"
@@ -423,13 +428,13 @@ set /a "var1=randomString"
 SET var2=7 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress7"="7" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress7"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddress7"="7" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=0123456543012"
 set "length=1"
@@ -442,14 +447,14 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress8"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress8"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 
 ) ELSE (
     SET /A "NetworkAddress8"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 
 set "charset=01234543210123"
@@ -463,13 +468,13 @@ set /a "var1=%randomString%"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres7"="5" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres7"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddres7"="5" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set "charset=01234567890"
 set "length=1"
@@ -482,15 +487,15 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres6"="%randomString%"  
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres6"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 
 ) ELSE (
     SET /A "NetworkAddres6"="%randomString%" 
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 set "charset=012345432012"
@@ -504,14 +509,14 @@ set /a "var1=randomString"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddres5"="5"  
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddres5"="%randomString%"  
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 ) ELSE (
     SET /A "NetworkAddres5"="5"  
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 set "charset=01234543210123"
@@ -525,15 +530,15 @@ set /a "var1=%randomString%"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /a "NetworkAddressk"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /a "NetworkAddressk"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 
 ) ELSE (
     SET /a  "NetworkAddressk"="%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 set "charset=0123456789"
@@ -548,15 +553,15 @@ set /a "var1=%randomString%"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress16"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress16"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 
 ) ELSE (
     SET /A "NetworkAddress16"="%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 set "charset=012345678901"
@@ -571,15 +576,15 @@ set /a "var1=%randomString%"
 SET var2=5 
 IF %var1% GTR %var2% (
     SET /A "NetworkAddress17"="%randomString%" 
-    ECHO %var1% больше %var2%.
+    ECHO %var1% ГЎГ®Г«ГјГёГҐ %var2%.
 
 ) ELSE IF %var1% LSS %var2% (
     SET /A "NetworkAddress17"="%randomString%" 
-    ECHO %var1% меньше %var2%.
+    ECHO %var1% Г¬ГҐГ­ГјГёГҐ %var2%.
 
 ) ELSE (
     SET /A "NetworkAddress17"="%randomString%"
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 
 )
 echo %NetworkAddress9%%NetworkAddres8%:%NetworkAddres7%%NetworkAddres6%:%NetworkAddres5%%NetworkAddress17%,%NetworkAddressk%%NetworkAddress16%  
@@ -589,69 +594,70 @@ set /a "sar7=%NetworkAddress9%%NetworkAddres8%"
 SET sar8=23
 IF %sar7% GTR %sar7% (
     time 13:%NetworkAddres7%%NetworkAddres6%:%NetworkAddres5%%NetworkAddress17%,%NetworkAddressk%%NetworkAddress16%
-    ECHO %sar7% больше %sar8%.
+    ECHO %sar7% ГЎГ®Г«ГјГёГҐ %sar8%.
 ) ELSE IF %sar7% LSS %sar8% (
-    ECHO %sar7% меньше %sar8%.
+    ECHO %sar7% Г¬ГҐГ­ГјГёГҐ %sar8%.
 ) 
 set /a "sar9=%NetworkAddres7%%NetworkAddres6%"
 SET sar10=59
 IF %sar9% GTR %sar10% (
-    ECHO %sar9% больше %sar10%.
+    ECHO %sar9% ГЎГ®Г«ГјГёГҐ %sar10%.
 ) ELSE IF %sar9% LSS %sar10% (
     time %NetworkAddress9%%NetworkAddres8%:24:%NetworkAddres5%%NetworkAddress17%,%NetworkAddressk%%NetworkAddress16%
-    ECHO %sar9% меньше %sar10%.
+    ECHO %sar9% Г¬ГҐГ­ГјГёГҐ %sar10%.
 ) 
 set /a "sar11=%NetworkAddres5%%NetworkAddress17%"
 SET sar12=59
 IF %sar11% GTR %sar12% (
-    ECHO %sar11% больше %sar12%.
+    ECHO %sar11% ГЎГ®Г«ГјГёГҐ %sar12%.
 ) ELSE IF %sar11% LSS %sar12% (
     time %NetworkAddress9%%NetworkAddres8%:%NetworkAddres7%%NetworkAddres6%:33,%NetworkAddressk%%NetworkAddress16%
-    ECHO %sar11% меньше %sar12%.
+    ECHO %sar11% Г¬ГҐГ­ГјГёГҐ %sar12%.
 ) 
 set /a "sar13=%NetworkAddressk%%NetworkAddress16%"
 SET sar14=10
 IF %sar13% GTR %sar14% (
-    ECHO %sar13% больше %sar14%.
+    ECHO %sar13% ГЎГ®Г«ГјГёГҐ %sar14%.
 ) ELSE IF %sar13% LSS %sar14% (
     echo %NetworkAddress9%%NetworkAddres8%:%NetworkAddres7%%NetworkAddres6%:%NetworkAddres5%%NetworkAddress17%,%NetworkAddressk%%NetworkAddress16%
     time %NetworkAddress9%%NetworkAddres8%:%NetworkAddres7%%NetworkAddres6%:%NetworkAddres5%%NetworkAddress17%,%NetworkAddressk%%NetworkAddress16%
-    ECHO %sar13% меньше %sar14%.
+    ECHO %sar13% Г¬ГҐГ­ГјГёГҐ %sar14%.
 ) 
 ECHO %NetworkAddress1%%NetworkAddress2%-%NetworkAddress3%%NetworkAddress4%-%NetworkAddress5%%NetworkAddress6%%NetworkAddress7%%NetworkAddress8%
 set /a "sar1=%NetworkAddress1%%NetworkAddress2%"
 SET sar2=30 
 IF %sar1% GTR %sar2% (
-    ECHO %sar1% больше %sar2%.
+    ECHO %sar1% ГЎГ®Г«ГјГёГҐ %sar2%.
 
 ) ELSE IF %sar1% LSS %sar2% (
-    ECHO %sar1% меньше %sar2%.
+    ECHO %sar1% Г¬ГҐГ­ГјГёГҐ %sar2%.
 ) 
 set /a "sar1=%NetworkAddress1%%NetworkAddress2%"
 SET sar2=00 
 IF %sar1% GTR %sar2% (
-    ECHO %sar1% больше %sar2%.
+    ECHO %sar1% ГЎГ®Г«ГјГёГҐ %sar2%.
 
 ) ELSE IF %sar1% LSS %sar2% (
-    ECHO %sar1% меньше %sar2%.
+    ECHO %sar1% Г¬ГҐГ­ГјГёГҐ %sar2%.
 ) ELSE (
     date 23-03-2034
-    ECHO %var1% равно %var2%.
+    ECHO %var1% Г°Г ГўГ­Г® %var2%.
 )
 set /a "sar3=%NetworkAddress3%%NetworkAddress4%"
 SET sar4=12
 IF %sar3% GTR %sar4% (
-    ECHO %sar3% больше %sar4%.
+    ECHO %sar3% ГЎГ®Г«ГјГёГҐ %sar4%.
 
 ) ELSE IF %sar3% LSS %sar4% (
     ::date 14-%NetworkAddress3%%NetworkAddress4%-2002
-    ECHO %sar4% меньше %sar4%.
+    ECHO %sar4% Г¬ГҐГ­ГјГёГҐ %sar4%.
 ) 
 set /a "sar5=%NetworkAddress5%%NetworkAddress6%%NetworkAddress7%%NetworkAddress8%"
 SET sar6=1999
 IF %sar1% GTR %sar2% (
-    ECHO %sar1% больше %sar2%.
+    ECHO %sar1% ГЎГ®Г«ГјГёГҐ %sar2%.
 ) ELSE IF %sar1% LSS %sar2% (
-    ECHO %sar1% меньше %sar2%.
+    ECHO %sar1% Г¬ГҐГ­ГјГёГҐ %sar2%.
 ) 
+
 goto loop
